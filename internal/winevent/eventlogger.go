943f99660d570ccd8fec2de34f8a9731b1bc5db5
buu-huu <luca-schwarz@outlook.de>
2024-06-15 01:33:14 +0200

    Fix wrong sourcename in eventlogger

--- internal/winevent/eventlogger.go
+++ internal/winevent/eventlogger.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	WinEventSourceName = "PurrsomWatch"
+	WinEventSourceName string = "PurrsomWatch"
 )
 
 type SubSource int
@@ -26,23 +26,24 @@ func (s SubSource) String() string {
 // InstallWinEventSource TODO: Unexport function after testing
 func InstallWinEventSource() error {
 	for _, subSource := range []SubSource{System, Detection} {
-		err := eventlog.InstallAsEventCreate(subSource.String(), eventlog.Info|eventlog.Warning|eventlog.Error)
+		sourceToInstall := fmt.Sprintf("%s-%s", WinEventSourceName, subSource.String())
+		err := eventlog.InstallAsEventCreate(sourceToInstall, eventlog.Info|eventlog.Warning|eventlog.Error)
 		if err != nil {
 			// Trying to parse errors here to generic errors for proper logging...
 			var errno syscall.Errno
 			if errors.As(err, &errno) && errors.Is(errno, syscall.ERROR_ACCESS_DENIED) {
-				fmt.Printf("Error installing Wineventlog subsource %s. Insufficient permissions: %s\n", subSource.String(), syscall.ERROR_ACCESS_DENIED)
+				fmt.Printf("Error installing Wineventlog subsource %s. Insufficient permissions: %s\n", sourceToInstall, syscall.ERROR_ACCESS_DENIED)
 				return err
 			} else {
 				// Fall back to string of the error message if not a permission problem
 				if strings.Contains(err.Error(), "registry key already exists") {
-					fmt.Printf("It appears, that Wineventlog subsource %s is already registered/installed: %s\n", subSource.String(), err.Error())
+					fmt.Printf("It appears, that Wineventlog subsource %s is already registered/installed: %s\n", sourceToInstall, err.Error())
 				} else {
-					fmt.Printf("Unknown error registering/installing Wineventlog subsource %s: %s\n", subSource.String(), err.Error())
+					fmt.Printf("Unknown error registering/installing Wineventlog subsource %s: %s\n", sourceToInstall, err.Error())
 				}
 			}
 		} else {
-			fmt.Printf("Wineventlog subsource %s installed successfully.\n", subSource.String())
+			fmt.Printf("Wineventlog subsource %s installed successfully.\n", sourceToInstall)
 		}
 	}
 	return nil
