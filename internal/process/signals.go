5b2126212b4ed5a14e8f4bbcd84ab71bff39b895
buu-huu <luca-schwarz@outlook.de>
2024-06-16 01:06:20 +0200

    Add process handling and os signals

---
+++ internal/process/signals.go
@@ -0,0 +1,51 @@
+// MIT License
+//
+// Copyright (c) 2024 buuhuu
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Package process provides process level features such as OS signal handling
+package process
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	ExitDelay = 2000
+)
+
+// HandleProcessTermination handles OS signals which ask the process to shut down (e.g. when the system shuts down)
+// This closes all relevant queues, etc.
+func HandleProcessTermination(cleanupFunc func()) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP)
+
+	go func() {
+		sig := <-sigChan
+		fmt.Println("Received OS signal:", sig)
+		cleanupFunc()
+		time.Sleep(ExitDelay * time.Millisecond)
+		os.Exit(0)
+	}()
+}
